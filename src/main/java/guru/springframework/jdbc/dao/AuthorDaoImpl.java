e98386f710b5913d16ed956386acdc5ba87537dd
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-20 20:42:39 +0300

    Creates the author row mapper

---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -0,0 +1,38 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AuthorDaoImpl implements AuthorDao {
+    @Override
+    public Author getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Author findAuthorByName(String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+
+    }
+
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
+    }
+}
